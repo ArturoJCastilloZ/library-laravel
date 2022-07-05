be59cf7e02ab419f899ccb8617306b104e374c1d
ArturoJCastilloZ <castillo.arturo93@hotmail.com>
2022-07-05 17:18:49 -0500

    Se redirecciona directamente a Login y se elimina el archivo Welcome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,14 +7,14 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
+                <div class="card-body text-center fs-1">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('BIENVENIDO'.' '.Auth::user()->name.' !' ) }}
                 </div>
             </div>
         </div>
